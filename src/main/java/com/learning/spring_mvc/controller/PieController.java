161ea288e5dd582ba41414e639c08078e873fca1
beastthor <beastthor.ykc@gmail.com>
2025-12-04 12:15:37 +0530

    running jar alone

--- src/main/java/com/learning/spring_mvc/controller/PieController.java
+++ src/main/java/com/learning/spring_mvc/controller/PieController.java
@@ -1,13 +1,24 @@
 package com.learning.spring_mvc.controller;
 
 
+import com.learning.spring_mvc.model.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class PieController {
+
+    private List<Product> pies = new ArrayList<>(List.of(
+            new Product(1,"Apple"),
+            new Product(2,"cherry")
+    ));
 
     @GetMapping("products/{id}")
     public Map<String,Object> getProduct(@PathVariable int id){
@@ -28,9 +39,33 @@ public class PieController {
     }
 
     @PostMapping("/products")
-    public Map<String,Object> addProduct(@RequestBody Map<String,Object> newProduct){
+    public Product addProduct(@RequestBody Product newProduct){
         System.out.println(newProduct);
         return newProduct;
     }
+
+    @PutMapping("/products/{id}")
+    public Product updateProduct(@PathVariable int id, @RequestBody Product updatedProduct){
+        Product existingProduct = pies.stream()
+                .filter(p -> p.getId()==id)
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Pie not found"));
+
+        existingProduct.setName(updatedProduct.getName());
+        existingProduct.setId(updatedProduct.getId());
+        return existingProduct;
+    };
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable int id){
+        Product existingProduct = pies.stream()
+                .filter(p -> p.getId()==id)
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Pie not found"));
+        pies.remove(existingProduct);
+        return ResponseEntity.noContent().build();
+    };
+
+
 
 }
--- src/test/java/com/learning/spring_mvc/SpringMvcApplicationTests.java
+++ src/test/java/com/learning/spring_mvc/SpringMvcApplicationTests.java
@@ -1,13 +1,41 @@
 package com.learning.spring_mvc;
 
+import com.learning.spring_mvc.model.Product;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+//Static Import have to set manually
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import java.util.Map;
+
+
+// what is webenvironment tells us , we are going to call same url that are current application is running on and defined in spring configuration
+@SpringBootTest(webEnvironment =SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SpringMvcApplicationTests {
 
+	private final RestTemplate restTemplate = new RestTemplate();
+
+
+	@Disabled
 	@Test
-	void contextLoads() {
+	void getSingleProductById() {
+		String url = "https://localhost:8080/api/products/1";
+		Map response = restTemplate.getForObject(url,Map.class);
+
+		assertThat(response).isNotNull();
+		assertThat(response.get("id")).isEqualTo(1);
+		assertThat(response.get("name")).isEqualTo("Product 1");
+	}
+
+	@Disabled
+	@Test
+	void addProduct(){
+		String url = "http://localhost:8080/api/products";
+		Product newProduct = new Product(1,"apple");
+		Product response = restTemplate.postForObject(url,newProduct, Product.class);
+		assertThat(response).isNotNull();
 	}
 
 }
