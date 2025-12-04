21195dde644c61cd17f0e42ba0e658e940151790
beastthor <beastthor.ykc@gmail.com>
2025-12-04 06:55:03 +0530

    first push

---
+++ src/test/java/com/learning/spring_mvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
